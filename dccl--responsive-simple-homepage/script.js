baf8b58a006958fdf633213a6452b3a75ecbb156
Elane Alencar <160562648+elanealencar@users.noreply.github.com>
2024-06-17 20:43:40 -0300

    Add files via upload

--- dccl--responsive-simple-homepage/script.js
+++ dccl--responsive-simple-homepage/script.js
@@ -1,9 +1,20 @@
+// Selecionando elementos necessários e verificando sua existência
     const body = document.body;
     const imgElement = document.querySelector('.moon');
-    const logoElement = document.querySelector('logo');
+    const logoElement = document.querySelector('.logo');
 
+// Função para alternar o modo escuro
     function toggleDarkMode() {
     body.classList.toggle('dark-mode');
-    imgElement.src ='assets/images/Moon_fill.svg';
-    logoElement.src ='assets/images/alarado-icon-homepage-dark.svg';
+
+// Verificando se os elementos existem antes de alterar suas propriedades
+    if (imgElement) {
+        imgElement.src = 'assets/images/Moon_fill.svg';
     }
+    if (logoElement) {
+        logoElement.src = 'assets/images/alarado-icon-homepage-dark.svg';
+    }
+    }
+
+
+     
